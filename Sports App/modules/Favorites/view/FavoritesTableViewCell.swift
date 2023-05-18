9220e91c3e94d5d7a2ab0c7a8ab9e3471159c2cd
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-05-18 15:13:19 +0300

    prepare view
    prepare view

---
+++ Sports App/modules/Favorites/view/FavoritesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoritesTableViewCell.swift
+//  Sports App
+//
+//  Created by Mac on 18/05/2023.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Sports App/modules/Favorites/viewModel/FavoritesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesViewModel.swift
+//  Sports App
+//
+//  Created by Mac on 18/05/2023.
+//
+
+import Foundation
