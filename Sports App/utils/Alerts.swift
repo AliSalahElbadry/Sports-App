e7b3d9569eb360124638e057582a385b0bd1e6b6
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-05-18 14:11:37 +0300

    updating
    updating

--- Sports App/utils/Alerts.swift
+++ Sports App/utils/Alerts.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import UIKit
+
+class Alerts{
+    private var instance:Alerts?
+    func getInstance()->Alerts{
+        if instance == nil{
+            instance = Alerts()
+        }
+        return instance!
+    }
+    private func showAlert(msg:String){
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertController.Style.alert)
+        alert.present(animated: true, completion: nil)
+    }
+    private func showAlert(msg:String,complitionHandeler:(() -> Void)?)
+    {
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertController.Style.alert)
+        alert.present(animated: true, completion: complitionHandeler)
+    }
+}
+extension UIAlertController {
+    
+    func show() {
+        present(animated: true, completion: nil)
+    }
+    
+    func present(animated: Bool, completion: (() -> Void)?) {
+        if let rootVC = UIApplication.shared.keyWindow?.rootViewController {
+            presentFromController(controller: rootVC, animated: animated, completion: completion)
+        }
+    }
+    
+    private func presentFromController(controller: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        if
+            let navVC = controller as? UINavigationController,
+            let visibleVC = navVC.visibleViewController
+        {
+            presentFromController(controller: visibleVC, animated: animated, completion: completion)
+        } else if
+            let tabVC = controller as? UITabBarController,
+            let selectedVC = tabVC.selectedViewController
+        {
+            presentFromController(controller: selectedVC, animated: animated, completion: completion)
+        } else if let presented = controller.presentedViewController {
+            presentFromController(controller: presented, animated: animated, completion: completion)
+        } else {
+            controller.present(self, animated: animated, completion: completion);
+        }
+    }
+}
